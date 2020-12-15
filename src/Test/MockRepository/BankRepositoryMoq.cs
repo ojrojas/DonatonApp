6ef6f2fab84b97e5e9b5334250dc6dc8fbb6d605
Oscar Rojas <orojasgarces1@gmail.com>
2020-12-14 22:23:46 -0500

    add test project

---
+++ src/Test/MockRepository/BankRepositoryMoq.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace Test.MockRepository
+{
+    public class BankRepositoryMoq
+    {
+        public Mock<IBankService> _bankRepository;
+
+        public BankRepositoryMoq()
+        {
+            _bankRepository = new Mock<IBankService>();
+            ConfiguracionBankRepositoryMoq();
+        }
+
+        private void ConfiguracionBankRepositoryMoq()
+        {
+            //Update
+            _bankRepository.Setup((x) => x.Edit(
+                It.IsAny<Bank>())).ReturnsAsync(default(Bank));
+            //Create
+            _bankRepository.Setup((x) => x.Create(
+                It.IsAny<Bank>())).ReturnsAsync(default(Bank));
+            //Delete
+            _bankRepository.Setup((x) => x.Delete(
+                It.IsAny<Guid>())).ReturnsAsync(default(Bank));
+            //Get
+            _bankRepository.Setup((x) => x.GetAllBanks()).ReturnsAsync(default(List<Bank>));
+        }
+    }
+}
