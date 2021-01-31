6e43f97f09ff6da56db60b51720e8073a3e1dc5f
OSCAR JULIAN ROJAS <orojasgarces@outlook.com>
2021-01-31 15:18:56 -0500

    fix controller bank

--- src/Api/Controllers/BanksController.cs
+++ src/Api/Controllers/BanksController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Api.Controllers
 {
     [ApiController]
-    // [Authorize]
+    [Authorize]
     [AllowAnonymous]
     [Route("api/[controller]")]
     public class BanksController : ControllerBase
